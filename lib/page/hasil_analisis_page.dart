8a99fc99458ba4c24d05ae4709bbd00d341e75e9
hafizzibrahim <hafizzibrahimm0@gmail.com>
2024-04-29 20:00:52 +0700

    update layou

--- lib/page/hasil_analisis_page.dart
+++ lib/page/hasil_analisis_page.dart
@@ -10,6 +10,93 @@ class HasilAnalisisPage extends StatelessWidget {
         backgroundColor: greenColor,
         title: Text('HASIL ANALISIS', style: whiteTextStyle.copyWith(fontWeight: bold, fontSize: 20),),
       ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        children: [
+          hasilContainer(),
+          SizedBox(height: 16,),
+          hasilContainer()
+        ],
+      ),
+    );
+  }
+
+  Widget hasilContainer(){
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+      decoration: BoxDecoration(
+        border: Border.all(color: greenColor),
+        borderRadius: BorderRadius.circular(14),
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Kapasitas (C)', style: blackTextStyle.copyWith(fontSize: 12, fontWeight: medium),),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 8),
+                width: 120,
+                decoration: BoxDecoration(
+                  border: Border.all(color: greenColor),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child:Text('', textAlign: TextAlign.center,),
+              ),
+            ],
+          ),
+          SizedBox(height: 16,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Kecepatan Arus Bebas (VB)', style: blackTextStyle.copyWith(fontSize: 12, fontWeight: medium),),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 8),
+                width: 120,
+                decoration: BoxDecoration(
+                  border: Border.all(color: greenColor),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child:Text('', textAlign: TextAlign.center,),
+              ),
+            ],
+          ),
+          SizedBox(height: 16,),
+          Table(
+              border: TableBorder.all(),
+              children: [
+                TableRow(
+                    children :[
+                      Text('Tundaan (Detik)', textAlign: TextAlign.center,),
+                      Text('Panjang Antrian (M)', textAlign: TextAlign.center),
+                      Text('VT (Km/Jam)', textAlign: TextAlign.center,),
+                      Text('Tingkat Pelayanan', textAlign: TextAlign.center),
+                    ]),
+                TableRow(
+                    children :[
+                      Text('', textAlign: TextAlign.center,),
+                      Text('', textAlign: TextAlign.center,),
+                      Text('', textAlign: TextAlign.center,),
+                      Text('', textAlign: TextAlign.center),
+                    ]),
+                TableRow(
+                    children :[
+                      Text('', textAlign: TextAlign.center,),
+                      Text('', textAlign: TextAlign.center,),
+                      Text('', textAlign: TextAlign.center,),
+                      Text('', textAlign: TextAlign.center),
+                    ]),
+                TableRow(
+                    children :[
+                      Text('', textAlign: TextAlign.center,),
+                      Text('', textAlign: TextAlign.center,),
+                      Text('', textAlign: TextAlign.center,),
+                      Text('', textAlign: TextAlign.center),
+                    ]),
+              ]
+          ),
+        ],
+      ),
     );
   }
 }
