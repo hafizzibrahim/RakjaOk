5d1cdeb93da0853989a8e6de68aee8ee9ec2fe77
hafizzibrahim <hafizzibrahimm0@gmail.com>
2024-04-29 15:10:09 +0700

    add part

--- lib/page/pages.dart
+++ lib/page/pages.dart
@@ -1,5 +1,8 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:rakja_ok/shared/shared.dart';
 import '../widget/widgets.dart';
 
@@ -11,4 +14,5 @@ part 'analisis_jalan_page.dart';
 part 'kecepatan_rata_page.dart';
 part 'hasil_analisis_page.dart';
 part 'informasi_page.dart';
+part 'analisis_hari_page.dart';
 part 'main_page.dart';
