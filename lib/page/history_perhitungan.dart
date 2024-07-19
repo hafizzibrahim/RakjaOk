e29e8bd19a935b8ebdf911693fbcfbb27a282c2a
Hafiz Ibrahim <hafizzibrahimm0@gmail.com>
2024-07-19 09:50:51 +0700

    add page history_perhitungan

---
+++ lib/page/history_perhitungan.dart
@@ -0,0 +1,11 @@
+part of 'pages.dart';
+
+class HistoryPerhitungan extends StatelessWidget {
+  const HistoryPerhitungan({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
